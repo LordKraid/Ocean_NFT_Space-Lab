eeea23d101f69a68f099e95921402f75fe36b4ab
Drop <the.lord.kraid@gmail.com>
2022-10-27 12:33:14 +0300

    add explore button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swipercardFlex = new Swiper('.card-flex', {
+const swiperCardFlex = new Swiper('.card-flex', {
     breakpoints: {
         992: {
             slidesPerView: 3,
@@ -12,6 +12,24 @@ const swipercardFlex = new Swiper('.card-flex', {
     },
 
     navigation: {
-        nextEl: '.next'
+        nextEl: '.card__explore'
+    }
+});
+
+const swiperUserFlex = new Swiper('.users__card-flex', {
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+        },
+        576: {
+            slidesPerView: 2.1,
+        },
+        0: {
+            slidesPerView: 1.1,
+        }
+    },
+
+    navigation: {
+        nextEl: '.user__explore'
     }
 });
